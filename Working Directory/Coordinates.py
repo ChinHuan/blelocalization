a6d5f18925b9d8581593a587737a558c719bdfce
Chin Huan Tan <chinhuan99@gmail.com>
2019-12-13 11:35:49 +0800

    Add test data collection

--- Working Directory/Coordinates.py
+++ Working Directory/Coordinates.py
@@ -8,7 +8,7 @@ fig, ax = plt.subplots(figsize=(15,15))
 ax.imshow(img)
 
 def onclick(event):
-    print("X: {}, Y: {}".format(event.xdata / 44.0, event.ydata / 44.0))
+    print("{:.6f},{:.6f}".format(event.xdata / 44.0, event.ydata / 44.0))
 
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
