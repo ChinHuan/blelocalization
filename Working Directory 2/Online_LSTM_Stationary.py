3804378d00a1dbb43db37827f4aa4d5c8f9de480
Chin Huan Tan <chinhuan99@gmail.com>
2020-02-04 14:04:47 +0800

    Commit

---
+++ Working Directory 2/Online_LSTM_Stationary.py
@@ -0,0 +1,61 @@
+from utilities import scanners, format_data, min_max_scaling, read_ble
+from utilities import InfluxDBClient, AnimatedScatter
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+import tensorflow as tf
+from sklearn.preprocessing import normalize
+from datetime import timedelta
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+def create_window(dataset, win_size, start_index=0, end_index=None):
+    data = []
+    start_index = start_index + win_size
+    if end_index is None:
+        end_index = len(dataset)
+
+    for i in range(start_index, end_index):
+        indices = range(i-win_size, i)
+        slices = dataset.iloc[indices]
+        if slices.index.max() - slices.index.min() < timedelta(milliseconds=win_size * 300):
+            data.append(slices[scanners].values)
+    return np.array(data)
+
+def impute(data):
+    data.update(data.groupby(pd.Grouper(freq='100ms')).mean())
+    data.update(data.rolling('5s').mean())
+    data.ffill(inplace=True)
+
+def linear_impute(data):
+    for s in scanners:
+        dx = coor[0] - ble[s]['X']
+        dy = coor[1] - ble[s]['Y']
+        d = np.sqrt(np.square(dy) + np.square(dx))
+        reg = joblib.load('../Models/linear_distance_models_2/{}.joblib'.format(s))
+        ss = pd.Series(reg.predict(d.reshape((-1, 1)))).repeat(data.shape[0])
+        ss.index = data.index
+        data[s].fillna(ss, inplace=True)
+
+def predict():
+    global coor
+    data = client.retrieveData(seconds=10, beacon="0117C55D14E4")
+    data = format_data(data)
+    impute(data)
+    linear_impute(data)
+    data[scanners] = normalize(data[scanners] + 100)
+    data = create_window(data, 10)
+    coor = model.predict(data).mean(axis=0).mean(axis=0)
+    print(coor)
+    return np.expand_dims(coor, axis=0)
+
+ble = read_ble('../Data/ble.csv')
+coor = np.array([0, 0])
+model = tf.keras.models.load_model("../Models/LSTM_Stationary_W10.h5")
+client = InfluxDBClient()
+
+ani = AnimatedScatter(predict)
+plt.show()
